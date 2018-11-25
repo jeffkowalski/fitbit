161ec59733923468ba13d6ad149273ee78e5bec8
Jeff Kowalski <jeff.kowalski@gmail.com>
2018-11-24 21:07:06 -0800

    correct issues reported by rubocop; add more description to readme

--- Gemfile
+++ Gemfile
@@ -1,10 +1,10 @@
 source 'https://rubygems.org'
 
-gem 'thor'
 gem 'fitgem_oauth2'
 gem 'influxdb'
+gem 'thor'
 
 # for robe:
+gem 'method_source', '>=0.8.2'
 gem 'pry'
 gem 'pry-doc', '>=0.6.0'
-gem 'method_source', '>=0.8.2'
--- fitbit.rb
+++ fitbit.rb
@@ -5,20 +5,20 @@ require 'fileutils'
 require 'logger'
 require 'yaml'
 require 'fitgem_oauth2'
-require 'date'
 require 'influxdb'
+require 'time'
 
 LOGFILE = File.join(Dir.home, '.log', 'fitbit.log')
 CREDENTIALS_PATH = File.join(Dir.home, '.credentials', 'fitbit.yaml')
 
 class Fitbit < Thor
-  no_commands {
+  no_commands do
     def redirect_output
       unless LOGFILE == 'STDOUT'
         logfile = File.expand_path(LOGFILE)
-        FileUtils.mkdir_p(File.dirname(logfile), :mode => 0755)
+        FileUtils.mkdir_p(File.dirname(logfile), mode: 0o755)
         FileUtils.touch logfile
-        File.chmod 0644, logfile
+        File.chmod 0o644, logfile
         $stdout.reopen logfile, 'a'
       end
       $stderr.reopen $stdout
@@ -28,15 +28,15 @@ class Fitbit < Thor
     def setup_logger
       redirect_output if options[:log]
 
-      $logger = Logger.new STDOUT
-      $logger.level = options[:verbose] ? Logger::DEBUG : Logger::INFO
-      $logger.info 'starting'
+      @logger = Logger.new STDOUT
+      @logger.level = options[:verbose] ? Logger::DEBUG : Logger::INFO
+      @logger.info 'starting'
     end
-  }
+  end
 
-  class_option :log,     :type => :boolean, :default => true, :desc => "log output to #{LOGFILE}"
-  class_option :verbose, :type => :boolean, :aliases => '-v', :desc => 'increase verbosity'
-  class_option :dry_run, :type => :boolean, :aliases => '-d', :desc => 'do not write to database'
+  class_option :log,     type: :boolean, default: true, desc: "log output to #{LOGFILE}"
+  class_option :verbose, type: :boolean, aliases: '-v', desc: 'increase verbosity'
+  class_option :dry_run, type: :boolean, aliases: '-d', desc: 'do not write to database'
 
   desc 'record-status', 'record the current data to database'
   def record_status
@@ -44,16 +44,16 @@ class Fitbit < Thor
 
     credentials = YAML.load_file CREDENTIALS_PATH
 
-    client = FitgemOauth2::Client.new client_id:     credentials[:client_id],
+    client = FitgemOauth2::Client.new client_id: credentials[:client_id],
                                       client_secret: credentials[:client_secret],
-                                      token:         credentials[:access_token],
-                                      user_id:       credentials[:user_id],
-                                      unit_system:   'en_US'
+                                      token: credentials[:access_token],
+                                      user_id: credentials[:user_id],
+                                      unit_system: 'en_US'
 
-    records = client.weight_logs start_date: Date.today, period: '7d'
+    records = client.weight_logs start_date: Time.now.strftime('%Y-%m-%d'), period: '7d'
     influxdb = InfluxDB::Client.new 'fitbit'
 
-    records['weight'].each { |rec|
+    records['weight'].each do |rec|
       # {"bmi"=>21.21,
       #  "date"=>"2018-10-04",
       #  "fat"=>18.981000900268555,
@@ -61,21 +61,22 @@ class Fitbit < Thor
       #  "source"=>"Aria",
       #  "time"=>"14:51:16",
       #  "weight"=>66.6}
-      utc_time = (DateTime.parse rec['date'] + ' ' + rec['time']).to_time.to_i - Time.now.utc_offset
+      utc_time = Time.parse(rec['date'] + ' ' + rec['time']).to_i
+
       data = {
         values: { value: rec['weight'].to_f },
         timestamp: utc_time
       }
-      $logger.info "weight: #{data}"
-      influxdb.write_point('weight', data)
+      @logger.info "weight: #{data}"
+      influxdb.write_point('weight', data) unless options[:dry_run]
 
       data = {
-        values: { value: rec['fat'].to_f},
+        values: { value: rec['fat'].to_f },
         timestamp: utc_time
       }
-      $logger.info "fat: #{data}"
-      influxdb.write_point('fat', data)
-    }
+      @logger.info "fat: #{data}"
+      influxdb.write_point('fat', data) unless options[:dry_run]
+    end
   end
 end
 
